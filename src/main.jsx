a804a17552c23607abee31d95363c03e6a095d19
sarathchandrabajjanki <sarathchandrabajjanki@gmail.com>
2025-10-15 18:19:24 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Verification from './components/auth/verification.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Verification/>
   </StrictMode>,
 )
